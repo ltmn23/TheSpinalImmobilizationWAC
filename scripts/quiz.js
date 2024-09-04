a2c24ee52b42329ed8d4442f1dc5468d61ce1a09
ltmn23 <lea.tromont@gmail.com>
2024-09-04 09:36:59 +0200

    Upgrade
    quiz.js corrected
    
    Immobilisation -> Immobilization

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -20,62 +20,39 @@ let countdown;
 const quizArray = [
     {
         id: "0",
-        question: "Which is the most widely spoken language in the world?",
-        options: ["Spanish", "Mandarin", "English", "German"],
-        correct: "Mandarin",
+        question: "When was the cervical collar removed from ambulances in Belgium?",
+        options: ["1 June 2023", "1 January 2024", "1 March 2024"],
+        correct: "1 January 2024",
     },
     {
         id: "1",
-        question: "Which is the only continent in the world without a desert?",
-        options: ["North America", "Asia", "Africa", "Europe"],
-        correct: "Europe",
+        question: "What is the harmful effect of the cervical collar?",
+        options: ["Aesthetics", "Head maintenance", "Blood absorption"],
+        correct: "Head maintenance",
     },
     {
         id: "2",
-        question: "Who invented Computer?",
-        options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
-        correct: "Charles Babbage",
+        question: "What is the detrimental effect of the BaXstrap?",
+        options: ["The space it takes up in ambulances", "It is too flat and therefore not anatomical", "It requires a lot of people to handle"],
+        correct: "It is too flat and therefore not anatomical",
     },
     {
         id: "3",
-        question: "What do you call a computer on a network that requests files from another computer?",
-        options: ["A client", "A host", "A router", "A web server"],
-        correct: "A client",
+        question: "What replaces the cervical collar?",
+        options: ["Adhesive tape", "A towel", "Head blocks"],
+        correct: "Head blocks",
     },
     {
         id: "4",
-        question: "Hardware devices that are not part of the main computer system and are often added later to the system.",
-        options: ["Peripheral", "Clip art", "Highlight", "Execute"],
-        correct: "Peripheral",
+        question: "What replaces the BaXstrap?",
+        options: ["Stretcher", "The scoop", "A seat"],
+        correct: "The scoop",
     },
     {
         id: "5",
-        question: "The main computer that stores the files that can be sent to computers that are networked together is:",
-        options: ["Clip art", "Mother board", "Peripheral", "File server"],
-        correct: "File server",
-    }, {
-        id: "6",
-        question: "How can you catch a computer virus?",
-        options: ["Sending e-mail messages", "Using a laptop during the winter", "Opening e-mail attachments", "Shopping on-line"],
-        correct: "Opening e-mail attachments",
-    },
-    {
-        id: "7",
-        question: "Google (www.google.com) is a:",
-        options: ["Search Engine", "Number in Math", "Directory of images", "Chat service on the web"],
-        correct: "Search Engine",
-    },
-    {
-        id: "8",
-        question: "Which is not an Internet protocol?",
-        options: ["HTTP", "FTP", "STP", "IP"],
-        correct: "STP",
-    },
-    {
-        id: "9",
-        question: "Which of the following is not a valid domain name?",
-        options: ["www.yahoo.com", "www.yahoo.co.uk", "www.com.yahoo", "www.yahoo.co.in"],
-        correct: "www.com.yahoo",
+        question: "Do you think there is equipment suitable for children without cervical collars?",
+        options: ["YES", "NO", "/"],
+        correct: "NO",
     },
 ];
 
@@ -168,7 +145,6 @@ function quizCreator() {
     <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
      <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
       <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
-       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
     `;
         quizContainer.appendChild(div);
     }
@@ -227,4 +203,3 @@ window.onload = () => {
     startScreen.classList.remove("hide");
     displayContainer.classList.add("hide");
 };
-
